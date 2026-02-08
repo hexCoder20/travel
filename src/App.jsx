60a2d934050d84247980f69b9faffd3637cf6e29
unknown <hexCoder20@users.noreply.github.com>
2026-02-08 16:33:04 +0500

    feat: implement hero section and transparent navbar

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <Navbar />
+    <Hero />
+     </> 
+  )
+}
+
+export default App
---
+++ src/components/Hero.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Hero.css';
+
+
+function Hero() {
+    return (
+        <div className='hero'>
+            <div className="hero-content">
+                <h1>Discover The World</h1>
+                <p>Experience the most beautiful places on earth.</p>
+                <button className="cta-button">Start Your Journey</button>
+            </div>
+        </div>
+    )
+}
+
+export default Hero;
